d4ee01d4eb0d6484df1d43bee22a548d2d58394b
Milo Padilla-Cardona <emiliano98padillac@gmail.com>
2020-04-30 19:56:50 +0000

    Terminal commands for git cash added

--- Training.R
+++ Training.R
@@ -1,1 +1,16 @@
-asdf 
+
+##TERMINAL COMMANDS
+
+#Connect GitHub email to RStudio
+# git config --global user.email "myemail@email.com"
+# git config --global user.name "My Name"
+
+#Change cache timeout
+# git config --global credential.helper 'cache --timeout 3600' (FOR ONE HOUR TIMEOUT)
+
+#Link Repository to GitHub
+# 1. Create a New Repository on GitHub with same name as local project. Ignore extra files
+# 2. In terminal, change working directory to local project: pwd command yields present working directory
+#     cd is command to change directory followed by new direction
+# 3. git init > git add. > git commit -m "initial commit"
+# 4. copy and paste code from GitGub to link repository through Terminal.
